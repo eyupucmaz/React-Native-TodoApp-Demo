dbd1316010011cf83147cf66995b1859edb6bfd3
eyupucmaz <eyupucmaz@gmail.com>
2020-08-08 19:16:29 +0300

    add navigate

--- src/Router.js
+++ src/Router.js
@@ -1,20 +1,40 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import {Image} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import AddTodo from './Screens/AddTodo';
 import Todos from './Screens/Todos';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 const Stack = createStackNavigator();
 
 const Router = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerTintColor: 'black',
+          headerStyle: {backgroundColor: '#CFCFCF'},
+        }}
+        initialRouteName="Todos">
         <Stack.Screen
           name="Todos"
           component={Todos}
-          options={{title: 'My Todos'}}
+          options={({navigation, route}) => ({
+            title: 'Todo List',
+            headerRight: () => (
+              <TouchableOpacity
+                onPress={() => navigation.navigate('Add')}
+                style={{
+                  marginRight: 30,
+                }}>
+                <Image
+                  style={{height: 30, width: 30}}
+                  source={require('./images/file.png')}
+                />
+              </TouchableOpacity>
+            ),
+          })}
         />
         <Stack.Screen
           name="Add"
